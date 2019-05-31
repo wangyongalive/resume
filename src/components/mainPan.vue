6529933b7c77d290f030b674de2a99cc3bae119e
wangyongaliveuser.email <jsonwang0102@163.com>
2019-05-31 16:44:06 +0800

    ''

--- src/components/mainPan.vue
+++ src/components/mainPan.vue
@@ -5,18 +5,34 @@
       <dd>
         <h4> 网络结构交互式可视分析系统 2017.11~2018.12</h4>
         <h5>西南科技大学虚拟现实与可视化实验室</h5>
-        <p>角色：主研</p>
+        <p><h5 style="display: inline">角色</h5> 主研</p>
         <p>
-          技术栈：整个系统采用B/S架构，后台采用python+flask，网络特征参数计算中采用了networkx开发库，节点布局采用了igraph开发库，前端采用d3.js，包含五大模块：控制视图、主视图、统计视图、时间轴视图和表格视图。</p>
+        <h5 style="display: inline">项目背景：</h5>
+        该项目是北京大学委托开发的《网络综合态势展示和可视化分析技术研究》。
+        </p>
+        <p>
+        <h5 style="display: inline"> 技术栈：</h5>
+        整个系统采用B/S架构，后台采用python+flask，网络特征参数计算中采用了networkx开发库，节点布局采用了igraph开发库，前端采用d3.js，包含五大模块：控制视图、主视图、统计视图、时间轴视图和表格视图。</p>
         <img src="../assets/系统界面.png">
+        <p style="margin: 0 auto;width: 210px">网络拓扑结构可视化系统截图</p>
       </dd>
       <dd>
         <h4> 网络安全可视化 2018.12~至今</h4>
         <h5>西南科技大学虚拟现实与可视化实验室</h5>
-        <p>角色：参研</p>
-        <p>任务：利用webgl展示多层网络模型</p>
-        <p>效果：能在较短时间能将模型绘制出来，且交互无卡顿</p>
+        <p>
+        <h5 style="display: inline"> 角色：</h5>
+          参研
+        </p>
+        <p>
+        <h5 style="display: inline">项目背景：</h5>
+        北大、上海交大、西科大共同合作
+        </p>
+        <p>
+        <h5 style="display: inline">任务：</h5>
+          利用webgl进行多层网络布局
+        </p>
         <img src="../assets/网络安全截图.png">
+        <p style="margin: 0 auto;width: 210px">网络安全可视化系统截图</p>
       </dd>
     </dl>
   </div>
